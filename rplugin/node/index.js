39a1a8a5a0d5d7242a07d104adc26f78d8c62a16
Stefan Watt <stefan.watt@gmail.com>
2022-07-15 21:41:19 +0200

    added logic for very basic typing test

--- rplugin/node/index.js
+++ rplugin/node/index.js
@@ -1,12 +1,15 @@
 const linesToString = (lines) => lines.reduce((previous, current) => `${previous}\n${current}`, '');
-
+const sampleText = "This is some sample text!"
 module.exports = plugin => {
+  const print = (text)=>{
+    plugin.nvim.lua(`print('${text}')`)
+  }
   const opts= {pattern:'*'};
   const {nvim} = plugin;
   plugin.registerAutocmd('TextChangedI',async()=>{
   const buf = await nvim.buffer;
     const lines = await buf.lines
     const bufText = linesToString(lines)
-    nvim.lua(`print('buffer contains ${bufText.length} chars')`)
+    if (bufText === sampleText) print("success")
   },opts)
 }
