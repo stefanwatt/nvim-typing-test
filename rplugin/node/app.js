5621f1e79157218047b0ee7eea2bd556b2b5e5fc
Stefan Watt <stefan.watt@gmail.com>
2022-07-17 22:09:58 +0200

    foo

--- rplugin/node/app.js
+++ rplugin/node/app.js
@@ -8,64 +8,54 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const levenshtein_1 = require("./levenshtein");
-const stopwatch_node_1 = require("stopwatch-node");
-const opts = { pattern: '*' };
-const linesToString = (lines) => (lines.length === 1 ? lines[0] : lines.reduce((previous, current) => `${previous}\n${current}`, '')).trim();
-const template = `
-  const compareBufferTextToTemplate = async ()=>{
-    const buf = await nvim.buffer;
-    const lines = await buf.lines
-    const bufText = linesToString(lines)
-    print(\`\${distanceAsPercentage(bufText,template)}% similarity\`)
-  }
-`.trim();
-const stopwatch = new stopwatch_node_1.StopWatch();
 module.exports = (plugin) => {
     const print = (text) => {
         plugin.nvim.lua(`print('${text}')`);
     };
-    const startTypingTest = () => __awaiter(void 0, void 0, void 0, function* () {
-        stopwatch.start();
-        setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
-            if (!stopwatch.isRunning())
-                return;
-            const bufText = yield getBufText();
-            const distanceAsPercentage = (0, levenshtein_1.getDistanceAsPercentage)(bufText, template);
-            print(statusText(distanceAsPercentage));
-        }), 1000);
-    });
-    const completeTest = () => {
-        stopwatch.stop();
-        print(`Test completed in ${Math.round(stopwatch.getTotalTime() / 1000)} seconds`);
-    };
-    const statusText = (distanceAsPercentage) => {
-        if (!stopwatch.isRunning())
-            return 'Test not started';
-        else {
-            return `${stopwatch.shortSummary()} - ${distanceAsPercentage}% similarity`;
-        }
-    };
-    const getBufText = () => __awaiter(void 0, void 0, void 0, function* () {
-        const buf = yield nvim.buffer;
-        const lines = yield buf.lines;
-        const bufText = linesToString(lines);
-        return bufText;
-    });
-    const compareBufferTextToTemplate = () => __awaiter(void 0, void 0, void 0, function* () {
-        if (!stopwatch.isRunning())
-            return;
-        const bufText = yield getBufText();
-        const distanceAsPercentage = (0, levenshtein_1.getDistanceAsPercentage)(bufText, template);
-        if (distanceAsPercentage === 100) {
-            completeTest();
-            return;
-        }
-        print(statusText(distanceAsPercentage));
-    });
+    // const startTypingTest = async ()=>{
+    //   stopwatch.start()
+    //   setInterval(async()=>{
+    //     if (!stopwatch.isRunning()) return
+    //     const bufText = await getBufText()
+    //     const distanceAsPercentage = getDistanceAsPercentage(bufText, template)
+    //     print(statusText(distanceAsPercentage))
+    //   },1000)
+    // }
+    //
+    // const completeTest = ()=>{
+    //   stopwatch.stop()
+    //   print(`Test completed in ${Math.round(stopwatch.getTotalTime()/1000)} seconds`)
+    // }
+    //
+    // const statusText = (distanceAsPercentage:number)=> {
+    //   if (!stopwatch.isRunning())
+    //     return 'Test not started'
+    //   else {
+    //     return `${stopwatch.shortSummary()} - ${distanceAsPercentage}% similarity`
+    //   }
+    // }
+    //
+    // const getBufText = async ()=>{
+    //   const buf = await nvim.buffer;
+    //   const lines = await buf.lines
+    //   const bufText = linesToString(lines)
+    //   return bufText
+    // }
+    //
+    // const compareBufferTextToTemplate = async ()=>{
+    //   if (!stopwatch.isRunning()) return;
+    //   const bufText = await getBufText()
+    //   const distanceAsPercentage = getDistanceAsPercentage(bufText,template)
+    //   if(distanceAsPercentage === 100) {
+    //     completeTest()
+    //     return
+    //   }
+    //   print(statusText(distanceAsPercentage))
+    // }
+    //
     const { nvim } = plugin;
-    plugin.registerCommand('TypingTestStart', startTypingTest, { sync: false });
-    plugin.registerAutocmd('TextChangedI', compareBufferTextToTemplate, opts);
-    plugin.registerAutocmd('TextChanged', compareBufferTextToTemplate, opts);
+    plugin.registerCommand('TypingTestStart', () => __awaiter(void 0, void 0, void 0, function* () { print('typing test started'); }), { sync: false });
+    // plugin.registerAutocmd('TextChangedI',compareBufferTextToTemplate,opts)
+    // plugin.registerAutocmd('TextChanged',compareBufferTextToTemplate,opts)
 };
 //# sourceMappingURL=app.js.map
--- rplugin/node/app.ts
+++ rplugin/node/app.ts
@@ -1,67 +1,67 @@
 import * as neovim from 'neovim'
 import { AutocmdOptions } from 'neovim/lib/host/NvimPlugin'
-import { getDistanceAsPercentage } from './levenshtein';
-import {StopWatch} from 'stopwatch-node'
-
-const opts:AutocmdOptions = {pattern:'*'};
-const linesToString = (lines:string[]) => (lines.length === 1 ? lines[0] : lines.reduce((previous, current) => `${previous}\n${current}`, '')).trim();
-const template = `
-  const compareBufferTextToTemplate = async ()=>{
-    const buf = await nvim.buffer;
-    const lines = await buf.lines
-    const bufText = linesToString(lines)
-    print(\`\${distanceAsPercentage(bufText,template)}% similarity\`)
-  }
-`.trim()
-const stopwatch = new StopWatch()
-
+// import { getDistanceAsPercentage } from './levenshtein';
+// import {StopWatch} from 'stopwatch-node'
+//
+// const opts:AutocmdOptions = {pattern:'*'};
+// const linesToString = (lines:string[]) => (lines.length === 1 ? lines[0] : lines.reduce((previous, current) => `${previous}\n${current}`, '')).trim();
+// const template = `
+//   const compareBufferTextToTemplate = async ()=>{
+//     const buf = await nvim.buffer;
+//     const lines = await buf.lines
+//     const bufText = linesToString(lines)
+//     print(\`\${distanceAsPercentage(bufText,template)}% similarity\`)
+//   }
+// `.trim()
+// const stopwatch = new StopWatch()
+//
 export = (plugin:neovim.NvimPlugin)=>{
   const print = (text:string|number)=>{
     plugin.nvim.lua(`print('${text}')`)
   }
-  const startTypingTest = async ()=>{
-    stopwatch.start()
-    setInterval(async()=>{
-      if (!stopwatch.isRunning()) return
-      const bufText = await getBufText()
-      const distanceAsPercentage = getDistanceAsPercentage(bufText, template)
-      print(statusText(distanceAsPercentage))
-    },1000)
-  }
-
-  const completeTest = ()=>{
-    stopwatch.stop()
-    print(`Test completed in ${Math.round(stopwatch.getTotalTime()/1000)} seconds`)
-  }
-
-  const statusText = (distanceAsPercentage:number)=> {
-    if (!stopwatch.isRunning())
-      return 'Test not started'
-    else {
-      return `${stopwatch.shortSummary()} - ${distanceAsPercentage}% similarity`
-    }
-  }
-
-  const getBufText = async ()=>{
-    const buf = await nvim.buffer;
-    const lines = await buf.lines
-    const bufText = linesToString(lines)
-    return bufText
-  }
-
-  const compareBufferTextToTemplate = async ()=>{
-    if (!stopwatch.isRunning()) return;
-    const bufText = await getBufText()
-    const distanceAsPercentage = getDistanceAsPercentage(bufText,template)
-    if(distanceAsPercentage === 100) {
-      completeTest()
-      return
-    }
-    print(statusText(distanceAsPercentage))
-  }
-
+  // const startTypingTest = async ()=>{
+  //   stopwatch.start()
+  //   setInterval(async()=>{
+  //     if (!stopwatch.isRunning()) return
+  //     const bufText = await getBufText()
+  //     const distanceAsPercentage = getDistanceAsPercentage(bufText, template)
+  //     print(statusText(distanceAsPercentage))
+  //   },1000)
+  // }
+  //
+  // const completeTest = ()=>{
+  //   stopwatch.stop()
+  //   print(`Test completed in ${Math.round(stopwatch.getTotalTime()/1000)} seconds`)
+  // }
+  //
+  // const statusText = (distanceAsPercentage:number)=> {
+  //   if (!stopwatch.isRunning())
+  //     return 'Test not started'
+  //   else {
+  //     return `${stopwatch.shortSummary()} - ${distanceAsPercentage}% similarity`
+  //   }
+  // }
+  //
+  // const getBufText = async ()=>{
+  //   const buf = await nvim.buffer;
+  //   const lines = await buf.lines
+  //   const bufText = linesToString(lines)
+  //   return bufText
+  // }
+  //
+  // const compareBufferTextToTemplate = async ()=>{
+  //   if (!stopwatch.isRunning()) return;
+  //   const bufText = await getBufText()
+  //   const distanceAsPercentage = getDistanceAsPercentage(bufText,template)
+  //   if(distanceAsPercentage === 100) {
+  //     completeTest()
+  //     return
+  //   }
+  //   print(statusText(distanceAsPercentage))
+  // }
+  //
   const {nvim} = plugin;
-  plugin.registerCommand('TypingTestStart', startTypingTest,{sync:false})
-  plugin.registerAutocmd('TextChangedI',compareBufferTextToTemplate,opts)
-  plugin.registerAutocmd('TextChanged',compareBufferTextToTemplate,opts)
+  plugin.registerCommand('TypingTestStart', async ()=>{ print('typing test started')},{sync:false})
+  // plugin.registerAutocmd('TextChangedI',compareBufferTextToTemplate,opts)
+  // plugin.registerAutocmd('TextChanged',compareBufferTextToTemplate,opts)
 }
