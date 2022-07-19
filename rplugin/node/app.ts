38b0bdf1707a8bafb9e9222d1e477f4debea5abc
Stefan Watt <stefan.watt@gmail.com>
2022-07-19 20:51:37 +0200

    added create buf cmd

--- rplugin/node/app.ts
+++ rplugin/node/app.ts
@@ -4,6 +4,7 @@ import { compareBufferTextToTemplate, quitTypingTest, startTypingTest } from './
 
 export = (plugin:neovim.NvimPlugin)=>{
   initNvim(plugin.nvim)
+  plugin.registerCommand('CreateBuf',async ()=>{plugin.nvim.createBuffer(false,true)},{sync:false})
   plugin.registerCommand('TypingTestStart',startTypingTest,{sync:false})
   plugin.registerCommand('TypingTestQuit',quitTypingTest,{sync:false})
   plugin.registerAutocmd('TextChangedI',compareBufferTextToTemplate,autoCmdOptions)
